bf44605747c01c3e704c143423e6ad3610afcd00
Cody Shaw <cek522@wildcats.unh.edu>
2021-01-05 11:17:25 -0500

    lesson 02

---
+++ app/src/main/java/com/codys/swoosh/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.codys.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
